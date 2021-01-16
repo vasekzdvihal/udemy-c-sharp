bb98da09e5c63c268016b39ec3df10ab1c56546e
Václav Zdvihal <zdvihal@gmail.com>
2021-01-16 21:07:45 +0100

    Fluent builder

---
+++ Builder.Fluent/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.Fluent
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement()
+        {
+
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImp(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+
+            sb.AppendLine($"{i}<{Name}>");
+
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImp(indent + 1));
+            }
+
+            sb.AppendLine($"{i}</{Name}>");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImp(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string rootName;
+        private HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var e = new HtmlElement(childName, childText);
+            root.Elements.Add(e);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = rootName };
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new HtmlBuilder("ul");
+            builder.AddChild("li", "hello").AddChild("li", "world");
+            Console.WriteLine(builder.ToString());
+        }
+    }
+}
---
+++ Builder.RecursiveGenerics/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder.RecursiveGenerics
+{
+    public class Person
+    {
+        public string Name;
+        public string Position;
+
+        public class Builder : PersonJobBuilder<Builder>
+        {
+
+        }
+
+        public static Builder New => new Builder();
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Position)}: {Position}";
+        }
+    }
+
+    public abstract class PersonBuilder
+    {
+        protected Person person = new Person();
+
+        public Person Build()
+        {
+            return person;
+        }
+    }
+
+
+    public class PersonInfoBuilder<SELF> : PersonBuilder where SELF : PersonInfoBuilder<SELF>
+    {
+        protected Person person = new Person();
+
+        public SELF Called(string name)
+        {
+            person.Name = name;
+            return (SELF)this;
+        }
+    }
+
+    public class PersonJobBuilder<SELF> : PersonInfoBuilder<PersonJobBuilder<SELF>> where SELF : PersonJobBuilder<SELF>
+    {
+        public SELF WorkAsA(string position)
+        {
+            person.Position = position;
+            return (SELF)this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var me = Person.New.Called("John").WorkAsA("Software Developer").Build();
+            Console.WriteLine(me);
+        }
+    }
+}
