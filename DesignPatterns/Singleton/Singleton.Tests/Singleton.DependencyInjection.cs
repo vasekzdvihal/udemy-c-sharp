cb78db5eb74eaba9a96594139494405fcccadb24
Václav Zdvihal <zdvihal@gmail.com>
2021-02-20 21:56:38 +0100

    Singleton, Singleton.DependencyInjection + Unit Tests for Singleton

---
+++ DesignPatterns/Singleton/Singleton.Tests/Singleton.DependencyInjection.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using NUnit.Framework;
+using Singleton.DependencyInjection;
+
+namespace Singleton.Tests
+{
+    [TestFixture]
+    public class Singleton_DependencyInjection
+    {   
+        [Test]
+        public void ConfigurablePopulationTest()
+        {
+            var rf = new ConfigurableRecordFinder(new DummyDatabase());
+            var names = new[] {"alpha", "gamma"};
+            int tp = rf.GetTotalPopulation(names);
+            Assert.That(tp, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void DIPopulationTest()
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<OrdinaryDatabase>()
+                .As<DependencyInjection.IDatabase>()
+                .SingleInstance();
+            cb.RegisterType<ConfigurableRecordFinder>();
+
+            using (var c = cb.Build())
+            {
+                var rf = c.Resolve<ConfigurableRecordFinder>();
+            }
+        }
+    }
+}
