e035a2012e6b8318a0609383625dc9590a85ae2d
Václav Zdvihal <zdvihal@gmail.com>
2021-02-21 14:28:24 +0100

    Adapter Caching, Generic Value, DI and Exercise

---
+++ DesignPatterns/Adapter.DI/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Features.Metadata;
+
+namespace Adapter.DI
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    class SaveCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Saving a file");
+        }
+    }
+    
+    class OpenCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Opening a file");
+        }
+    }
+
+    public class Button
+    {
+        private ICommand command;
+        private string name;
+        
+        public Button(ICommand command, string name)
+        {
+            this.command = command;
+            this.name = name;
+        }
+
+        public void Click()
+        {
+            command.Execute();
+        }
+
+        public void PrintMe()
+        {
+            Console.WriteLine($"I am a button called {name}");
+        }
+    }
+
+    public class Editor
+    {
+        private IEnumerable<Button> buttons;
+
+        public IEnumerable<Button> Buttons => buttons;
+
+        public Editor(IEnumerable<Button> buttons)
+        {
+            this.buttons = buttons;
+        }
+
+        public void ClickAll()
+        {
+            foreach (var btn in buttons)
+            {
+                btn.Click();
+            }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new ContainerBuilder();
+            b.RegisterType<SaveCommand>().As<ICommand>()
+                .WithMetadata("Name", "Save");
+            b.RegisterType<OpenCommand>().As<ICommand>()
+                .WithMetadata("Name", "Open");
+            //b.RegisterType<Button>();
+            // b.RegisterAdapter<ICommand, Button>(cmd => new Button(cmd));
+            b.RegisterAdapter<Meta<ICommand>, Button>(cmd =>
+                new Button(cmd.Value, (string)cmd.Metadata["Name"]));
+            b.RegisterType<Editor>();
+
+            using (var c = b.Build())
+            {
+                var editor = c.Resolve<Editor>();
+                // editor.ClickAll();
+                foreach (var btn in editor.Buttons)
+                {
+                    btn.PrintMe();
+                }
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Adapter.Exercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Adapter.Exercise
+{
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+    
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public SquareToRectangleAdapter(Square square)
+        {
+            Width = square.Side;
+            Height = square.Side;
+        }
+        
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO Vypsat na obrazovku to pouziti, na Udemy to proslo v pohode
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
