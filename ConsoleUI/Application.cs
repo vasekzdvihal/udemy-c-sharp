e288653984d740be765a3cc2c64783254d2c096a
Václav Zdvihal <zdvihal@gmail.com>
2021-02-24 10:19:24 +0100

    Other/Dependency Indjection with Autofac.dll

---
+++ ConsoleUI/Application.cs
@@ -0,0 +1,24 @@
+﻿using DemoLibrary;
+
+namespace ConsoleUI
+{
+    public interface IApplication
+    {
+        void Run();
+    }
+
+    public class Application : IApplication
+    {
+        IBusinessLogic _bussinesLogic;
+
+        public Application(IBusinessLogic businessLogic)
+        {
+            _bussinesLogic = businessLogic;
+        }
+
+        public void Run()
+        {
+            _bussinesLogic.ProcessData();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Autofac;
+using DemoLibrary;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = ContainerConfig.Configure();
+
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var app = scope.Resolve<IApplication>();
+                app.Run();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
