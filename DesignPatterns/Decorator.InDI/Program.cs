7ea96be2b827d4bbeab3cf49b2c7ec06c2fe692d
Václav Zdvihal <zdvihal@gmail.com>
2021-03-21 14:54:46 +0100

    Decorator in DI and Exercise

---
+++ DesignPatterns/Decorator.InDI/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Autofac;
+
+namespace Decorator.InDI
+{
+    public interface IReportingService
+    {
+        void Report();
+    }
+
+    public class ReportingService : IReportingService
+    {
+        public void Report()
+        {
+            Console.WriteLine("Here is your report.");
+        }
+    }
+    
+    public class ReportingServiceWithLogging : IReportingService
+    {
+        private IReportingService decorated;
+
+        public ReportingServiceWithLogging(IReportingService decorated)
+        {
+            this.decorated = decorated;
+        }
+        
+        public void Report()
+        {
+            Console.WriteLine("Commencing log...");
+            decorated.Report();
+            Console.WriteLine("Ending log...");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new ContainerBuilder();
+            b.RegisterType<ReportingService>().Named<IReportingService>("reporting");
+            b.RegisterDecorator<IReportingService>((context, service) => new ReportingServiceWithLogging(service),
+                "reporting");
+
+            using (var c = b.Build())
+            {
+                var r = c.Resolve<IReportingService>();
+                r.Report();
+            }
+        }
+    }
+}
---
+++ DesignPatterns/Decorator.StaticComposition/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Decorator.StaticComposition
+{
+    public abstract class Shape
+    {
+        public string AsString();
+    }
+    
+    public class TransparentShape : Shape
+    {
+        private Shape shape;
+        private float transparency;
+
+        public TransparentShape( Shape shape, float transparency)
+        {
+            this.shape = shape;
+            this.transparency = transparency;
+        }
+
+        public string AsString() => $"{shape.AsString()} has {transparency * 100.00} transparency";
+    }
+    
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
