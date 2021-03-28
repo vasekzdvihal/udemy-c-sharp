bf727197d3d3a5d12ae2c3849feb4a2deaaedbec
Václav Zdvihal <zdvihal@gmail.com>
2021-03-29 00:33:54 +0200

    Proxy Design Patter 1/x

---
+++ DesignPatterns/Proxy.Protection/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Proxy.Protection
+{
+    public interface ICar
+    {
+        void Drive();
+    }
+    
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is being driven");
+        }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Car car = new Car();
+        private Driver driver;
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+                car.Drive();
+            else
+            {
+                Console.WriteLine("Driver is too young.");
+            }
+        }
+    }
+
+    public class Driver
+    {
+        public int Age { get; set; }
+
+        public Driver(int age)
+        {
+            Age = age;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICar car = new CarProxy(new Driver(22));
+            car.Drive();
+        }
+    }
+}
