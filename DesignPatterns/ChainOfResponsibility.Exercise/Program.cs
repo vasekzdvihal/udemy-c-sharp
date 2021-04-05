847c2c1f9184026c0d2663a35ecb5b064f539e68
Václav Zdvihal <zdvihal@gmail.com>
2021-04-05 20:35:16 +0200

    Chain of Responsibility design pattern

---
+++ DesignPatterns/ChainOfResponsibility.Exercise/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChainOfResponsibility.Exercise
+{
+    public abstract class Creature
+    {
+        protected Game game;
+        protected int baseAttack;
+        protected int baseDefense;
+
+        protected Creature(Game game, int baseAttack, int baseDefense)
+        {
+            this.game = game;
+            this.baseAttack = baseAttack;
+            this.baseDefense = baseDefense;
+        }
+        
+        public virtual int Attack { get; set; }
+        public virtual int Defense { get; set; }
+        public abstract void Query(object source, StatQuery sq);
+
+        public override string ToString()
+        {
+            return $"{nameof(Attack)}: {Attack}, {nameof(Defense)}: {Defense}";
+        }
+    }
+
+    public class Goblin : Creature
+    {
+        public override void Query(object source, StatQuery sq)
+        {
+            if (ReferenceEquals(source, this))
+            {
+                switch (sq.Statistic)
+                {
+                    case Statistic.Attack:
+                        sq.Result += baseAttack;
+                        break;
+                    case Statistic.Defense:
+                        sq.Result += baseDefense;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+            }
+            else
+            {
+                if (sq.Statistic == Statistic.Defense)
+                {
+                    sq.Result++;
+                }
+            }
+        }
+
+        public override int Defense
+        {
+            get
+            {
+                var q = new StatQuery {Statistic = Statistic.Defense};
+                foreach(var c in game.Creatures)
+                    c.Query(this, q);
+                return q.Result;
+            }
+        }
+
+        public override int Attack
+        {
+            get
+            {
+                var q = new StatQuery {Statistic = Statistic.Attack};
+                foreach(var c in game.Creatures)
+                    c.Query(this, q);
+                return q.Result;
+            }
+        }
+
+        public Goblin(Game game) : base(game, 1, 1)
+        {
+            
+        }
+
+        protected Goblin(Game game, int baseAttack, int baseDefense) 
+            : base(game, baseAttack, baseDefense)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, {nameof(Defense)}: {Defense}, {nameof(Attack)}: {Attack}";
+        }
+    }
+
+    public class GoblinKing : Goblin
+    {
+        public GoblinKing(Game game) : base(game, 3,3)
+        { }
+
+        public override void Query(object source, StatQuery sq)
+        {
+            if (!ReferenceEquals(source, this) && sq.Statistic == Statistic.Attack)
+            {
+                sq.Result++; // every goblin gets +1 attack
+            }
+            else base.Query(source, sq);
+        }
+    }
+
+    public enum Statistic
+    {
+        Attack,
+        Defense
+    }
+    
+    public class StatQuery
+    {
+        public Statistic Statistic;
+        public int Result;
+    }
+    
+    public class Game
+    {
+        public IList<Creature> Creatures = new List<Creature>();
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            
+            var goblin = new Goblin(game);
+            game.Creatures.Add(goblin);
+            Console.WriteLine(goblin);
+
+            var goblinKing = new GoblinKing(game);
+            game.Creatures.Add(goblinKing);
+            Console.WriteLine(goblinKing);
+        }
+    }
+}
---
+++ DesignPatterns/ChainOfResponsibility.MethodChain/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace ChainOfResponsibility.MethodChain
+{
+    public class Creature
+    {
+        public string Name;
+        public int Attact, Defense;
+
+        public Creature(string name, int attact, int defense)
+        {
+            Name = name;
+            Attact = attact;
+            Defense = defense;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Attact)}: {Attact}, {nameof(Defense)}: {Defense}";
+        }
+    }
+
+    public class CreatureModifier
+    {
+        protected Creature creature;
+        protected CreatureModifier next; // linked list
+
+        public CreatureModifier(Creature creature)
+        {
+            this.creature = creature;
+        }
+
+        public void Add(CreatureModifier cm)
+        {
+            if (next != null) next.Add(cm);
+            else next = cm;
+        }
+
+        public virtual void Handle() => next?.Handle();
+    }
+
+    public class DoubleAttactModifier : CreatureModifier
+    {
+        public DoubleAttactModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Doubling {creature.Name}'s attack");
+            creature.Attact *= 2;
+            base.Handle();
+        }
+    }
+
+    public class IncresedDefenseModifier : CreatureModifier
+    {
+        public IncresedDefenseModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            Console.WriteLine($"Increasing {creature.Name}'s defense.");
+            creature.Defense += 3;
+            base.Handle();
+        }
+    }
+
+    public class NoBonusesModifier : CreatureModifier
+    {
+        public NoBonusesModifier(Creature creature) : base(creature)
+        {
+        }
+
+        public override void Handle()
+        {
+            //
+        }
+    }
+
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            var goblin = new Creature("Goblin", 2, 2);
+            Console.WriteLine(goblin);
+
+            var root = new CreatureModifier(goblin);
+            
+            Console.WriteLine("Let's double the goblin's attack");
+            root.Add(new DoubleAttactModifier(goblin));
+
+            root.Add(new NoBonusesModifier(goblin));
+            
+            Console.WriteLine("Let's increase the goblin's defense.");
+            root.Add(new IncresedDefenseModifier(goblin));
+            
+            root.Handle();
+            Console.WriteLine(goblin);
+        }
+    }
+}
