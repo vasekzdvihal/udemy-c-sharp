b1c85440c5ce020a41e62863f9b270122df82a74
Václav Zdvihal <zdvihal@gmail.com>
2021-05-01 22:28:31 +0200

    Design Patterns - Iterator, Duck Typing

---
+++ DesignPatterns/Iterator.ArrayBackProperties/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Iterator.ArrayBackProperties
+{
+    public class Creature : IEnumerable<int>
+    {
+        private int[] stats = new int[3];
+        private const int strenght = 0;
+
+        public int Strength
+        {
+            get => stats[strenght];
+            set => stats[strenght] = value;
+        }
+
+        public int Agility { get; set; }
+        public int Intelligence { get; set; }
+
+        public double AverageStat => stats.Average();
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return stats.AsEnumerable().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int this[int index]
+        {
+            get { return stats[index]; }
+            set { stats[index] = value; }
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
