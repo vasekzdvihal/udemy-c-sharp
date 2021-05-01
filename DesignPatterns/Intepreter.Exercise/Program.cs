ba132e05e783336653f8ab8e45315883338af1f6
Václav Zdvihal <zdvihal@gmail.com>
2021-05-01 22:08:45 +0200

    Design Patterns - Iterator, Object

--- DesignPatterns/Intepreter.Exercise/Program.cs
+++ DesignPatterns/Intepreter.Exercise/Program.cs
@@ -67,11 +67,8 @@ namespace Intepreter.Exercise
             ep.Variables.Add('x', 5);
 
             Assert.That(ep.Calculate("1"), Is.EqualTo(1));
-
             Assert.That(ep.Calculate("1+2"), Is.EqualTo(3));
-
             Assert.That(ep.Calculate("1+x"), Is.EqualTo(6));
-
             Assert.That(ep.Calculate("1+xy"), Is.EqualTo(0));
         }
     }
---
+++ DesignPatterns/Iterator.Object/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace Iterator.Object
+{
+    public class Node<T>
+    {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+
+            left.Parent = right.Parent = this;
+        }
+    }
+
+    public class InOrderIterator<T>
+    {
+        private readonly Node<T> root;
+        public Node<T> Current;
+        private bool yieldedStart;
+
+        public InOrderIterator(Node<T> root)
+        {
+            this.root = root;
+            Current = root;
+
+            while (Current.Left != null)
+                Current = Current.Left;
+            
+            //    1  <- root
+            //  /  \
+            //  2  3
+            //  ^ Current
+        }
+
+        public bool MoveNext()
+        {
+            if (!yieldedStart)
+            {
+                yieldedStart = true;
+                return true;
+            }
+
+            if (Current.Right != null)
+            {
+                Current = Current.Right;
+                while (Current.Left != null)
+                    Current = Current.Left;
+                return true;
+            }
+            else
+            {
+                var p = Current.Parent;
+                while (p != null && Current == p.Right)
+                {
+                    Current = p;
+                    p = p.Parent;
+                }
+
+                Current = p;
+                return Current != null;
+            }
+        }
+
+        public void Reset()
+        {
+            Current = root;
+            yieldedStart = false;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //    1 
+            //  /  \
+            //  2  3
+            // in-order: 213
+
+            var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
+
+            var it = new InOrderIterator<int>(root);
+            while (it.MoveNext())
+            {
+                Console.Write(it.Current.Value);
+                Console.Write(",");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
