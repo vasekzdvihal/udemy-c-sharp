61e0fe02c5172ba67f5a03e235a0666b43d445bb
Václav Zdvihal <zdvihal@gmail.com>
2021-05-13 20:52:36 +0200

    Design Patterns, Observer - Event keyword

---
+++ DesignPatterns/Observer.EventKeyword/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Observer.EventKeyword
+{
+    public class FallsIllEventArgs 
+    {
+        public string Address;
+    }
+    
+    public class Person
+    {
+        public void CatchACold()
+        {
+            FallsIll?.Invoke(this, new FallsIllEventArgs() { Address = "123 London Road"});
+        }
+        
+        public event EventHandler<FallsIllEventArgs> FallsIll;
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.FallsIll += CallDoctor;
+            person.CatchACold();
+        }
+
+        private static void CallDoctor(object? sender, FallsIllEventArgs e)
+        {
+            Console.WriteLine($"Doctor has been called to {e.Address}");
+        }
+    }
+}
