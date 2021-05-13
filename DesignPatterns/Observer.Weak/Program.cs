abaeffa2c547a2ad3d411374b140b56b19642260
Václav Zdvihal <zdvihal@gmail.com>
2021-05-13 21:17:47 +0200

    Design Patterns, Observer - Weak event

---
+++ DesignPatterns/Observer.Weak/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace Observer.WeakEventPattern
+{
+    public class Button
+    {
+        public event EventHandler Clicked;
+
+        public void Fire()
+        {
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    public class Window
+    {
+        public Window(Button button)
+        {
+            // Mel by to byt WindowsBase nugget ale nemuzu ho nikde najit
+            // WeakEventManager<Button, EventArgs>()
+            //     .AddHandler(button, "Clicked", ButtonOnClicked);
+        }
+
+        private void ButtonOnClicked(object sender, EventArgs eventArgs)
+        {
+            Console.WriteLine("Button clicked (Window handler)");
+        }
+
+        ~Window()
+        {
+            Console.WriteLine("Window finalized");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var button = new Button();
+            var window = new Window(button);
+
+            var windowRef = new WeakReference(window);
+            
+            button.Fire();
+            Console.WriteLine("Setting window to null");
+            window = null;
+            
+            FireGC();
+            Console.WriteLine($"Is the window alive after GC? {windowRef.IsAlive}");
+        }
+
+        private static void FireGC()
+        {
+            Console.WriteLine("Starting GC");
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+            Console.WriteLine("GC is done");
+        }
+    }
+}
