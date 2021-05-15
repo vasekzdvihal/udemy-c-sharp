25ce918de0cb9210f43e72c44f0369b75ac4f075
Václav Zdvihal <zdvihal@gmail.com>
2021-05-15 10:30:57 +0200

    Design Patterns, Oberver - Collections, 1

---
+++ DesignPatterns/Observable.Collections/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Channels;
+using Observable.Collections.Annotations;
+
+namespace Observable.Collections
+{
+    public class Market
+    {
+        public BindingList<float> Prices = new BindingList<float>();
+
+        public void AddPrice(float price)
+        {
+            Prices.Add(price);
+        }
+        
+    }
+    
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var market = new Market();
+
+            market.Prices.ListChanged += (sender, eventArgs) =>
+            {
+                if (eventArgs.ListChangedType == ListChangedType.ItemAdded)
+                {
+                    float price = ((BindingList<float>) sender)[eventArgs.NewIndex];
+                    Console.WriteLine($"Binding list got a price of {price}");
+                }
+            };
+            
+            market.AddPrice(123);
+        }
+    }
+}
---
+++ DesignPatterns/Observable.Collections/_obsolete.cs
@@ -0,0 +1,46 @@
+﻿// using System;
+// using System.ComponentModel;
+// using System.Runtime.CompilerServices;
+// using Observable.Collections.Annotations;
+//
+// namespace Observable.Collections
+// {
+//     public class Market : INotifyPropertyChanged
+//     {
+//         private float valatility;
+//
+//         public float Valatility
+//         {
+//             get => valatility;
+//             set
+//             {
+//                 if (value.Equals(valatility)) return;
+//                 valatility = value;
+//                 OnPropertyChanged();
+//             }
+//         }
+//
+//         public event PropertyChangedEventHandler PropertyChanged;
+//
+//         [NotifyPropertyChangedInvocator]
+//         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+//         {
+//             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+//         }
+//     }
+//     
+//     static class Program
+//     {
+//         public static void Main(string[] args)
+//         {
+//             var market = new Market();
+//             market.PropertyChanged += (sender, eventArgs) =>
+//             {
+//                 if (eventArgs.PropertyName == "Volatility")
+//                 {
+//                     Console.WriteLine("Volatility was changed");
+//                 }
+//             };
+//         }
+//     }
+// }
