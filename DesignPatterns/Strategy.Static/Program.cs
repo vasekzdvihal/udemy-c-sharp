7d9817922be9fd6c1a4e1260323d45bd5fc66c3d
Václav Zdvihal <zdvihal@gmail.com>
2021-05-16 15:31:15 +0200

    Design Patterns - Strategy, Static

---
+++ DesignPatterns/Strategy.Static/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strategy.Static
+{
+    public enum OutputFormat
+    {
+        Markdown, Html
+    }
+
+    public interface IListStrategy
+    {
+        void Start(StringBuilder sb);
+        void End(StringBuilder sb);
+        void AddListItem(StringBuilder sb, string item);
+    }
+
+    public class HtmlListStrategy : IListStrategy
+    {
+        public void Start(StringBuilder sb)
+        {
+            sb.AppendLine("<ul>");
+        }
+
+        public void End(StringBuilder sb)
+        {
+            sb.AppendLine("</ul>");
+        }
+
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($"  <li>{item}</li>");
+        }
+    }
+
+    public class MarkdownListStrategy : IListStrategy
+    {
+        public void Start(StringBuilder sb)
+        {
+
+        }
+
+        public void End(StringBuilder sb)
+        {
+
+        }
+
+        public void AddListItem(StringBuilder sb, string item)
+        {
+            sb.AppendLine($" * {item}");
+        }
+    }
+
+    public class TextProcessor<LS> where LS: IListStrategy, new()
+    {
+        private StringBuilder sb = new StringBuilder();
+        private IListStrategy listStrategy = new LS();
+        
+        public void AppendList(IEnumerable<string> items)
+        {
+            listStrategy.Start(sb);
+            foreach(var item in items)
+                listStrategy.AddListItem(sb, item);
+            listStrategy.End(sb);
+        }
+
+        public StringBuilder Clear()
+        {
+            return sb.Clear();
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
+    
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tp = new TextProcessor<MarkdownListStrategy>();
+            tp.AppendList(new []{"foo", "bar", "baz"});
+            Console.WriteLine(tp);
+            
+            var tp2 = new TextProcessor<HtmlListStrategy>();
+            tp2.AppendList(new []{"foo", "bar", "baz"});
+            Console.WriteLine(tp2);
+        }
+    }
+}
