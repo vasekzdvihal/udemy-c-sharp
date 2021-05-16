b2fb9242eef52ee0c0437d8cdc4bd8e6cdc19afa
Václav Zdvihal <zdvihal@gmail.com>
2021-05-16 10:54:41 +0200

    Design Patterns - State, Switch Expression

---
+++ DesignPatterns/Behavioral.State/State.SwitchExpression/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace State.SwitchExpression
+{
+    enum Chest
+    {
+        Open, Closed, Locked
+    }
+
+    enum Action
+    {
+        Open, Close
+    }
+    
+    class Program
+    {
+        static Chest Manipulate(Chest chest, Action action, bool haveKey)
+            => (chest, action, haveKey)
+                switch
+                {
+                    (Chest.Locked, Action.Open, true) => Chest.Open,
+                    (Chest.Closed, Action.Open, _) => Chest.Open,
+                    (Chest.Open, Action.Close, true) => Chest.Locked,
+                    (Chest.Open, Action.Close, false) => Chest.Closed,
+
+                    _ => chest
+                };
+
+        static void Main(string[] args)
+        {
+            var chest = Chest.Locked;
+            Console.WriteLine($"Chest is {chest}");
+
+            chest = Manipulate(chest, Action.Open, true);
+            Console.WriteLine($"Chest is {chest}");
+
+            chest = Manipulate(chest, Action.Close, false);
+            Console.WriteLine($"Chest is {chest}");
+
+            chest = Manipulate(chest, Action.Close, false);
+            Console.WriteLine($"Chest is {chest}");
+        }
+    }
+}
